e0c5ba0bdf384e2e052f640e9e16a0331de88500
ggepenyan <ggepenyan@gmail.com>
2016-04-27 16:53:10 +0400

    TinyMCE blogposts

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ var express = require('express'),
 	multiparty = require('multiparty'),
 	path = require('path'),
 
+	Entities = require('html-entities').AllHtmlEntities,
+	
+	entities = new Entities(),
+
 	Promise = require("bluebird"),
 	readFile = Promise.promisify(require("fs").readFile),
 	writeFile = Promise.promisify(require("fs").writeFile),
@@ -13,43 +17,43 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	if(req.user) {
-		return models.Users.findOne({
-			where: {
-				id: req.user.id
+	if(!req.user) {
+		return res.render('index')
+	}
+	
+	return models.Users.findOne({
+		where: {
+			id: req.user.id
+		}
+	}).then(user => {
+		return models.Blogposts.findAll({
+			where:{
+				userid: user.id
 			}
-		}).then(user => {
-			return models.Blogposts.findAll({
-				where:{
-					userid: user.id
+		}).then(blogpost => {
+			return models.Comments.findAll({
+				where: {
+					password: null
 				}
-			}).then(blogpost =>{
-				return models.Comments.findAll({
-					where: {
-						password: null
-					}
-				}).then(comment => {
+			}).then(comment => {
 
-					res.render('user',  {
-						user: user,
-						blogpost: blogpost,
-						comment: comment,
-						isuser: true
-					})
+				res.render('user',  {
+					user: user,
+					blogpost: blogpost,
+					comment: comment,
+					isuser: true
 				})
 			})
-		}).catch(error => {
-			next(error)
 		})
-	} else{
-		res.render('index')
-	}
+	}).catch(error => {
+		next(error)
+	})
 })
 
 router.get('/register', function(req, res, next) {
 	if (req.user) {
 		req.flash('error', 'log out please')
-		res.redirect('/')
+		return res.redirect('/')
 	} else{
 		res.render('register')
 	}
@@ -58,314 +62,332 @@ router.get('/register', function(req, res, next) {
 router.post("/register", function (req, res, next) {
 	if (req.user) {
 		req.flash('error', 'log out please')
-		res.redirect('/')
-	} else {
-		
-		if (req.body.firstname == '' || req.body.lastname == '' || req.body.username == '' || req.body.password == '') {
-			req.flash('error', 'you must fill fields')
-			res.redirect('/register')
-
-		} else if (req.body.firstname && req.body.lastname && req.body.username && req.body.password) {
-
-			return models.Users.findOne({
-				where:{
-					username: req.body.username
-				}
-			}).then(user => {
-				if (user == null) {
-
-					return models.Users.create({
-						firstname: req.body.firstname,
-						lastname: req.body.lastname,
-						username: req.body.username,
-						password: req.body.password,
-						picture: "/images/profile.png"
-					}).then(user => {
-						res.redirect('/')
-					})
-				} else {
-					req.flash('error', 'user with that username exists')
-					res.redirect('/register')
-				}
-			}).catch(error => {
-				next(error)
-			})
-		} else {
-			req.flash('error', 'you must fill fields')
-			res.redirect('/register')
-		}
+		return res.redirect('/')
 	}
+
+	if (!req.body.firstname && !req.body.lastname && !req.body.username && !req.body.password) {
+		req.flash('error', 'you must fill fields')
+		return res.redirect('/register')
+	}
+
+	if (req.body.firstname == '' || req.body.lastname == '' || req.body.username == '' || req.body.password == '') {
+		req.flash('error', 'you must fill fields')
+		return res.redirect('/register')
+	} 
+
+	return models.Users.findOne({
+		where:{
+			username: req.body.username
+		}
+	}).then(user => {
+		if (user !== null) {
+			req.flash('error', 'user with that username exists')
+			return res.redirect('/register')
+		}
+		return models.Users.create({
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			username: req.body.username,
+			password: req.body.password,
+			picture: "/images/profile.png"
+		})
+
+	}).then(user => {
+		res.redirect('/')
+	}).catch(error => {
+		next(error)
+	})
 })
 
 router.post('/upload', function (req, res, next) {
 	if (!req.user) {
-		res.redirect('/')
-	} else {
-		var form = new multiparty.Form()
-		
-		form.parse(req, function (error, fields, files) {
-			if (files.image) {
-				if (files.image[0].originalFilename !== '' && files.image[0].size !== 0) {
-					var image = files.image[0]
-					var path_full = './public/images/' + image.originalFilename
-					
-					readFile(image.path).then(result => {
-						
-						return writeFile(path_full, result).then(error => {
-
-							ext = path.extname(image.originalFilename)
-							
-							new_path = './public/images/' + Math.random() + '-' + req.user.username + ext
-
-							path_for_db = new_path.slice(8, new_path.length)
-
-							return rename(path_full, new_path).then(result => {
-								return models.Users.update(
-									{picture: path_for_db},
-									{where: {
-										id: req.user.id
-									}
-								}).then(user => {
-									console.log(user)
-									return models.Blogposts.update(
-										{userimage: path_for_db},
-										{where: {
-											username: req.user.username
-										}
-									}).then(blog_user =>{
-										console.log("upload success")
-										res.redirect('/')
-									})
-								})
-							})
-						})
-					}).catch(error => {
-						next(error)
-					})
-				} else {
-					req.flash('error', 'not correct')
-					res.redirect('/change')
-				}
-			} else {
-				req.flash('error', 'you must fill field')
-				res.redirect('/change')
-			}
-		})
+		return res.redirect('/')
 	}
+	var form = new multiparty.Form()
+		
+	form.parse(req, function (error, fields, files) {
+		if (!files.image) {
+			req.flash('error', 'not correct')
+			return res.redirect('/change')
+		}
+		if (files.image[0].originalFilename === '' && files.image[0].size == 0) {
+			
+			req.flash('error', 'you must fill field')
+			return res.redirect('/change')
+		}
+		var image = files.image[0]
+		var path_full = './public/images/' + image.originalFilename
+		
+		readFile(image.path).then(result => {
+			
+			return writeFile(path_full, result).then(error => {
+
+				ext = path.extname(image.originalFilename)
+				
+				new_path = './public/images/' + Math.random() + '-' + req.user.username + ext
+
+				path_for_db = new_path.slice(8, new_path.length)
+
+				return rename(path_full, new_path).then(result => {
+					return models.Users.update(
+						{picture: path_for_db},
+						{where: {
+							id: req.user.id
+						}
+					}).then(user => {
+						console.log(user)
+						return models.Blogposts.update(
+							{userimage: path_for_db},
+							{where: {
+								username: req.user.username
+							}
+						}).then(blog_user =>{
+							console.log("upload success")
+							res.redirect('/')
+						})
+					})
+				})
+			})
+		}).catch(error => {
+			next(error)
+		})
+	})
 })
 
 router.get('/login', function(req, res, next) {
 	if (req.user) {
 		req.flash('error', 'please logout at first')
-		res.redirect('/')
-	}else {
-		res.render('login')
+		return res.redirect('/')
 	}
+	res.render('login')
 })
 
 router.get('/change', function (req, res, next) {
 	if (!req.user) {
 		req.flash('error', 'please login at first')
-		res.redirect('/login')
-	} else {
-		return models.Users.findOne({
-			where:{
-				id: req.user.id
-			}
-		}).then(user => {
-			res.render('change', {
-				user: user
-			})
-		
-		}).catch(error => {
-			next(error)
-		})
+		return res.redirect('/login')
 	}
+	return models.Users.findOne({
+		where:{
+			id: req.user.id
+		}
+	}).then(user => {
+		res.render('change', {
+			user: user
+		})
+	
+	}).catch(error => {
+		next(error)
+	})
 })
 
 router.post('/change', function (req, res, next) {
 	if (!req.user) {
 		req.flash('error', 'please login at first')
-		res.redirect('/login')
-	} else {
-		var user_dates = {
-			firstname:req.body.firstname,
-			lastname:req.body.lastname
-		}
-
-		if (req.body.firstname && req.body.lastname) {
-			if (user_dates.firstname == '' || user_dates.lastname == '') {
-
-				req.flash('error', 'you must fill fields')
-				res.redirect('/change')
-
-			} else {
-				return models.Users.findOne({
-					where: {
-						id: req.user.id
-					}
-				}).then(user => {
-					return models.Blogposts.findAll({
-						where: {
-							userid: user.id
-						}
-					}).then(blog_user =>{
-						user.update({
-							firstname: user_dates.firstname || req.user.firstname,
-							lastname: user_dates.lastname || req.user.lastname
-						})
-
-						blog_user.map(function (serial_user) {
-							serial_user.update({
-								firstname: user_dates.firstname || req.user.firstname,
-								lastname: user_dates.lastname || req.user.lastname
-							})
-						})
-						req.flash('info', 'account dates are updated')
-						res.redirect("/")
-					})
-
-				}).catch(error => {
-					console.log(error)
-					next(error)
-				})
-			}
-		} else {
-			req.flash('error', 'you must fill fields')
-			res.redirect('/change')
-		}
+		return res.redirect('/login')
 	}
+	var user_dates = {
+		firstname:req.body.firstname,
+		lastname:req.body.lastname
+	}
+
+	if (!user_dates.firstname && !user_dates.lastname) {
+		req.flash('error', 'you must fill fields')
+		return res.redirect('/change')	
+	}
+	if (user_dates.firstname == '' || user_dates.lastname == '') {
+
+		req.flash('error', 'you must fill fields')
+		return res.redirect('/change')
+	} else {
+		return models.Users.findOne({
+			where: {
+				id: req.user.id
+			}
+		}).then(user => {
+			return models.Blogposts.findAll({
+				where: {
+					userid: user.id
+				}
+			}).then(blog_user =>{
+				user.update({
+					firstname: user_dates.firstname || req.user.firstname,
+					lastname: user_dates.lastname || req.user.lastname
+				})
+
+				blog_user.map(function (serial_user) {
+					serial_user.update({
+						firstname: user_dates.firstname || req.user.firstname,
+						lastname: user_dates.lastname || req.user.lastname
+					})
+				})
+				res.redirect("/")
+			})
+		}).catch(error => {
+			console.log(error)
+			next(error)
+		})
+	}
+})
+
+router.get('/authors/:user_id', function (req, res, next) {
+	if (!req.user) {
+		req.flash('error', 'login please')
+		return res.redirect('/')
+	}
+	var user_id = parseInt(req.params.user_id, 10) || 0
+	
+	if (user_id == 0) {
+		req.flash('error', 'uncorrect!')
+		res.redirect('/')
+	}
+	return models.Users.findOne({
+		where:{id: user_id}
+	}).then(user => {
+
+		if (user.id == req.user.id) {
+			return res.redirect('/')
+		}
+
+		return models.Blogposts.findAll({
+			where:{userid: user_id}
+		}).then(blogpost => {
+			res.render('user', {
+				user: user,
+				blogpost: blogpost,
+				guest: true
+			})
+		})
+
+	}).catch(error => {
+		next(error)
+	})
 })
 
 router.get('/blogpost', function (req, res, next) {
 	if (!req.user) {
 		req.flash('error', 'please login at first')
-		res.redirect('/')
-	}else {
+		return res.redirect('/')
+	}
 
-		return models.Blogposts.findAll({
+	return models.Blogposts.findAll({
+		where:{
+			password: null
+		}
+	}).then(blogpost => {
+		return models.Comments.findAll({
 			where:{
 				password: null
 			}
-		}).then(blogpost => {
-			return models.Comments.findAll({
-				where:{
-					password: null
-				}
-			}).then(comment => {
-				res.render("blogposts",{
-					blogpost: blogpost,
-					comment: comment,
-					user: req.user
-				})
+		}).then(comment => {
+			res.render("blogposts",{
+				blogpost: blogpost,
+				comment: comment,
+				user: req.user
 			})
-
-		}).catch(error => {
-			next(error)
 		})
-	}
+
+	}).catch(error => {
+		next(error)
+	})
 })
 
 router.post('/blogpost', function (req, res, next) {
 	if (!req.user) {
 		req.flash('error', 'please login at first')
-		res.redirect('/')
-	}else {
-		if(req.body.text) {
-			if (req.body.text !== '') {
-				return models.Users.findOne({
-					where: {
-						id: req.user.id
-					}
-				}).then(user => {
-					return models.Blogposts.create({
-						firstname: user.firstname,
-						lastname: user.lastname,
-						username: user.username,
-						userimage: user.picture,
-						userid: user.id,
-						content: req.body.text
+		return res.redirect('/')
+	}
+	
+	if(!req.body.text) {
+		req.flash('error', 'you must enter text')
+		return res.redirect('/blogpost')
+	}
+	if (req.body.text == '') {
+		req.flash('error', 'you must enter text')
+		return res.redirect('/blogpost')
+	}
+	return models.Users.findOne({
+		where: {
+			id: req.user.id
+		}
+	}).then(user => {
+		console.log(entities.encode(req.body.text))
+		return models.Blogposts.create({
+			firstname: user.firstname,
+			lastname: user.lastname,
+			username: user.username,
+			userimage: user.picture,
+			userid: user.id,
+			content: req.body.text
 
-					}).then(blogpost => {
-						req.flash('info', 'blogpost added successfully')
-						res.redirect("blogpost")					
-					})
-				}).catch(error => {
-					next(error)
-				})
-			} else {
-				req.flash('error', 'you must enter text')
-				res.redirect('/blogpost')
-			}
-		}
-		else {
-			req.flash('error', 'you must enter text')
-			res.redirect('/blogpost')
-		}
-	} 
+		}).then(blogpost => {
+			req.flash('info', 'blogpost added successfully')
+			res.redirect("/blogpost")					
+		})
+	}).catch(error => {
+		next(error)
+	})
 })
 
 router.post('/comment', function (req, res, next) {
 	if (!req.user) {
-		req.flash('error', 'please login at first')
-	} else {
-
-		if(req.body.comment_cont) {
-			if (req.body.comment_cont !== '') {
-				return models.Users.findOne({
-					where:{
-						id: req.user.id
-					}
-				}).then(user => {
-					return models.Comments.create({
-						blogpostid: req.body.blog_id,
-						userid: user.id,
-						firstname: user.firstname,
-						lastname: user.lastname,
-						userimage: user.picture,
-						content: req.body.comment_cont
-					}).then(comment => {
-						res.redirect('/blogpost')
-					})
-				}).catch(error => {
-					next(error)
-				})
-			} else {
-				req.flash('error', 'you must enter comment')
-				res.redirect('/blogpost')
-			}
-		} else{
-			req.flash('error', 'you must enter comment')
-			res.redirect('/blogpost')
-		}
+		return req.flash('error', 'please login at first')
 	}
+
+	if(!req.body.comment_cont) {
+		req.flash('error', 'you must enter comment')
+		return res.redirect('/blogpost')
+	}
+	
+	if (req.body.comment_cont == '') {
+		req.flash('error', 'you must enter comment')
+		return res.redirect('/blogpost')
+	}
+	
+	return models.Users.findOne({
+		where:{
+			id: req.user.id
+		}
+	}).then(user => {
+		return models.Comments.create({
+			blogpostid: req.body.blog_id,
+			userid: user.id,
+			firstname: user.firstname,
+			lastname: user.lastname,
+			userimage: user.picture,
+			content: req.body.comment_cont
+		}).then(comment => {
+			res.redirect('/blogpost')
+		})
+	}).catch(error => {
+		next(error)
+	})
 })
 
 router.post('/blogpost_remove', function (req, res, next) {
 	if (!req.user) {
 		req.flash('error', 'please login at first')
-		res.redirect('/')
-	} else {
-		return models.Blogposts.destroy({
-			where: {
-				id: req.body.blog_id,
-				userid: req.user.id
-			}
-		}).then(post =>{
-			return models.Comments.destroy({
-				where: {
-					blogpostid: req.body.blog_id,
-					// userid: req.user.id
-				}
-			}).then(comment => {
-				
-				req.flash('info', 'blogpost removed')
-				res.redirect('/blogpost')
-			})
-		}).catch(error =>{
-			next(error)
-		})
+		return res.redirect('/')
 	}
+	return models.Blogposts.destroy({
+		where: {
+			id: req.body.blog_id,
+			userid: req.user.id
+		}
+	}).then(post =>{
+		return models.Comments.destroy({
+			where: {
+				blogpostid: req.body.blog_id,
+				// userid: req.user.id
+			}
+		}).then(comment => {
+			
+			req.flash('info', 'blogpost removed')
+			res.redirect('/blogpost')
+		})
+	}).catch(error =>{
+		next(error)
+	})
 })
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -8,44 +8,4 @@ router.get('/', function(req, res, next) {
 	res.send("hello")
 })
 
-router.get('/:user_id', function (req, res, next) {
-	if (req.user) {
-
-		var user_id = parseInt(req.params.user_id, 10) || 0
-		if (user_id != 0) {
-
-			return models.Users.findOne({
-				where:{id: user_id}
-			}).then(user => {
-				if(user != undefined) {
-					if (user.id == req.user.id) {
-						return res.redirect('/')
-					} else {
-						return models.Blogposts.findAll({
-							where:{userid: user_id}
-						}).then(blogpost => {
-							res.render('user', {
-								user: user,
-								blogpost: blogpost,
-								guest: true
-							})
-						})
-					}
-				} else {
-					req.flash('error', 'Invalid user')
-					res.redirect('/')
-				}
-			}).catch(error => {
-				next(error)
-			})
-		} else {
-			req.flash('error', 'uncorrect!')
-			res.redirect('/')
-		}
-	} else {
-		req.flash('error', 'login please')
-		res.redirect('/')
-	}
-})
-
 module.exports = router;
